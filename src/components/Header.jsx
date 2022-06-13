e025d924c9dd86cf65bd9e3752fa3e1a2f9b69b6
sivaram2505 <79786425+sivaram2505@users.noreply.github.com>
2022-06-13 10:55:41 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightIcon />
+        Notes
+      </h1>
+    </header>
+  );
+}
